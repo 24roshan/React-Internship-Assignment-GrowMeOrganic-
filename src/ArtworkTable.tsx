151a5187903a702a05f0ce3bb5518c51d1bf1f38
Roshan <dearjha2019@gmail.com>
2026-01-11 14:51:00 +0530

    addremainingcodeofArtworkTable

--- src/ArtworkTable.tsx
+++ src/ArtworkTable.tsx
@@ -24,6 +24,41 @@ function ArtworkTable(props: Props) {
     props.onCustomSelect(num);
     overlayRef.current?.hide();
   };
-  
+  return (
+    <>
+      <Button
+        label="Custom Row Select"
+        onClick={(e) => overlayRef.current?.toggle(e)}
+        style={{ marginBottom: "10px" }}
+      />
+      <OverlayPanel ref={overlayRef}>
+        <div style={{ display: "flex", gap: "8px" }}>
+          <InputText onChange={(e) => (inputValue = e.target.value)} />
+          <Button label="Select" onClick={applyCustom} />
+        </div>
+      </OverlayPanel>
+      <DataTable
+        value={props.artworks}
+        paginator
+        lazy
+        rows={props.rowsPerPage}
+        totalRecords={props.total}
+        first={(props.page - 1) * props.rowsPerPage}
+        onPage={props.onPageChange}
+        selection={props.selectedRows}
+        onSelectionChange={(e) => props.onSelectionChange(e.value)}
+        dataKey="id"
+        selectionMode="checkbox"
+      >
+        <Column selectionMode="multiple" style={{ width: "3rem" }} />
+        <Column field="title" header="Title" />
+        <Column field="place_of_origin" header="Origin" />
+        <Column field="artist_display" header="Artist" />
+        <Column field="inscriptions" header="Inscriptions" />
+        <Column field="date_start" header="Start Year" />
+        <Column field="date_end" header="End Year" />
+      </DataTable>
+    </>
+  );
 }
 export default ArtworkTable;
